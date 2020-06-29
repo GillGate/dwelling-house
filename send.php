c64e60a619cf073cfdbf8ac8e66d42dca641978e
GillGate <gillgate@yandex.ru>
2020-06-29 16:38:46 +0300

    add ajax

--- send.php
+++ send.php
@@ -1,12 +1,17 @@
 <?php 
+	sleep(2);
 	$name  = trim($_POST['name']);
 	$phone = trim($_POST['phone']);
 
 	$date = date('Y-m-d H:i:s');
 
-	if($name !== '' & $phone !== '') {
-		file_put_contents('apps.txt', "date $name $phone \n", FILE_APPEND);
-		echo 'Success!';
-	} else {
-		echo 'Ну вы чего?'
+	if($name == '' || $phone == '') {
+		echo 'Заполните все поля!';
+	} elseif (strlen($name) < 2){
+		echo 'Имя должно содержать от 2 до 30 символов';
+	} elseif (!ctype_alpha($name)) {
+		echo 'Имя может содержать только буквы';
 	}
+	 else {
+    	echo 'ok';
+    }
